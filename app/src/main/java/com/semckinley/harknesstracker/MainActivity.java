def356e5b26e3e371bf17451ed279f62d4331eed
McKinn74 <mckinn.sm@gmail.com>
2018-01-26 21:25:08 -0500

    Added addactivity to add names. Added functional add button

--- app/src/main/java/com/semckinley/harknesstracker/MainActivity.java
+++ app/src/main/java/com/semckinley/harknesstracker/MainActivity.java
@@ -1,14 +1,19 @@
 package com.semckinley.harknesstracker;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements HarkAdapter.OnClickHandler{
 
     private static final int NUM_STUDENTS = 15;
     //Final form should not require this, but gets information from the data
@@ -17,27 +22,31 @@ public class MainActivity extends AppCompatActivity {
     private HarkAdapter mAdapter;
 
     private RecyclerView mStudentList;
+    private String[] mStudentInfoList;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        mStudentInfoList = intent.getStringArrayExtra("Student Names");
+
         //The above pretty much has to happen in all 'main activities' to get things rolling. At least as far as I know at this time
 
         mStudentList = (RecyclerView) findViewById(R.id.rv_students); //this creates the link between the java variable mStudentList and the Recyclerview in the layout xml file
 
         //below is the linearlayout manager set up that will help construct the recycler view?
 
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        GridLayoutManager layoutManager= new GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false);
         mStudentList.setLayoutManager(layoutManager);
         //This links the recyclerview to the layoutmanager I've chosen at this time. Initially linear.
-        //TODO change from linearlayout to gridlayout. May be a better layout choice.
-        //TODO Possibly make layout a settings choice for user preferences?
+
+
         mStudentList.setHasFixedSize(true);
         //going with true at first for simplicity sake, as it was in the example. However, classes can vary in size so may make this set to false in future
-
-        mAdapter = new HarkAdapter(NUM_STUDENTS); //The argument will not be needed once reading from data is accomplished
+        Log.i("tag", mStudentInfoList[5]);
+        mAdapter = new HarkAdapter(mStudentInfoList); //The argument will not be needed once reading from data is accomplished
         mStudentList.setAdapter(mAdapter);
 
     }
